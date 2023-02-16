3ccb2fcc49b5596b145782ece64d331f3af29e25
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-15 21:42:42 -0700

    minor updates to the skeleton of gamestateservice

--- stride-platformer/stride-platformer.Game/Services/GameStateService.cs
+++ stride-platformer/stride-platformer.Game/Services/GameStateService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Stride.Engine;
 
 namespace StridePlatformer.Services;
@@ -7,7 +8,10 @@ public class GameStateService
 	
 	public Game RunningGameData { get; }
 
-	public float PlayerHealth = 0;
+	public Entity Player { get; set; }
+	
+	//Coins registered in a scene
+	public List<Entity> CoinsInScene { get; private set; } = new();
 
 	
 	public GameStateService(Game game)
@@ -15,6 +19,9 @@ public class GameStateService
 		RunningGameData = game;
 	}
 
-
+	public void AddCoin(Entity coin)
+	{
+		CoinsInScene.Add(coin);
+	}
 
 }
