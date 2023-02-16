105a58f11e2180dd2a76ad2910ea4da70343dd89
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-15 21:40:07 -0700

    updated attack state to hopefully fix physics issue

--- stride-platformer/stride-platformer.Game/Core/AI/States/AttackState.cs
+++ stride-platformer/stride-platformer.Game/Core/AI/States/AttackState.cs
@@ -1,7 +1,10 @@
 
+using System.Collections.Specialized;
 using System.Threading.Tasks;
 using Doprez.Stride.AI.FSMs;
+using Stride.Core.Collections;
 using Stride.Engine;
+using Stride.Physics;
 
 public class AttackState : FSMState
 {
@@ -21,21 +24,38 @@ public class AttackState : FSMState
         _attackDuration = attackDuration;
     }
 
-    public override async Task EnterState()
+    public override Task EnterState() 
     {
-        _entitycollided = await _attackTrigger.GetCollidedEntity();
+		_attackTrigger.Collisions.CollectionChanged += CollisionsChanged;
+        return Task.CompletedTask;
     }
 
     public override Task ExitState()
     {
+		_attackTrigger.Collisions.CollectionChanged -= CollisionsChanged;
         return Task.CompletedTask;
     }
 
-    public override async Task UpdateState()
+    public override Task UpdateState() 
     {
-        if(_entitycollided != null && EntityToTryAndHit == _entitycollided)
-        {
-            //HurtPlayer
-        }
+		return Task.CompletedTask;
     }
+
+	private void CollisionsChanged(object sender, TrackingCollectionChangedEventArgs args) 
+    {
+		// Cast the argument 'item' to a collision object
+		var collision = (Collision)args.Item;
+
+		// We need to make sure which collision object is not the Trigger collider
+		// We perform a little check to find the ballCollider 
+		var playerCollider = _attackTrigger == collision.ColliderA ? collision.ColliderB : collision.ColliderA;
+
+		if (args.Action == NotifyCollectionChangedAction.Add) {
+			// When a collision has been added to the collision collection, we know an object has 'entered' our trigger
+			if (playerCollider.Entity.Name == "PlayerCharacter") {
+				_entitycollided = playerCollider.Entity;
+			}
+		}
+	}
+    
 }
