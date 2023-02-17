6c75aea41877ffda4bad634a1a78115693491c31
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-16 19:01:54 -0700

    code cleanup

--- stride-platformer/stride-platformer.Game/Controllers/CoinController.cs
+++ stride-platformer/stride-platformer.Game/Controllers/CoinController.cs
@@ -12,32 +12,33 @@ public class CoinController : StartupScript
 
 	public PhysicsComponent Trigger;
 
-    public override void Start() 
+	public override void Start() 
 	{
-        base.Start();
+		base.Start();
 		if(Trigger == null)
 		{
 			Trigger = Entity.Get<PhysicsComponent>();
 		}
 
 		Trigger.Collisions.CollectionChanged += CollisionsChanged;
-    }
+	}
 	
-    private void CollisionsChanged(object sender, TrackingCollectionChangedEventArgs args)
+	private void CollisionsChanged(object sender, TrackingCollectionChangedEventArgs args)
 	{
-        // Cast the argument 'item' to a collision object
-        var collision = (Collision)args.Item;
+		// Cast the argument 'item' to a collision object
+		var collision = (Collision)args.Item;
 
-        // We need to make sure which collision object is not the Trigger collider
-        // We perform a little check to find the ballCollider 
-        var playerCollider = Trigger == collision.ColliderA ? collision.ColliderB : collision.ColliderA;
+		// We need to make sure which collision object is not the Trigger collider
+		// We perform a little check to find the ballCollider 
+		var playerCollider = Trigger == collision.ColliderA ? collision.ColliderB : collision.ColliderA;
 
-        if (args.Action == NotifyCollectionChangedAction.Add) {
-            // When a collision has been added to the collision collection, we know an object has 'entered' our trigger
-            if(playerCollider.Entity.Name == "PlayerCharacter")
+		if (args.Action == NotifyCollectionChangedAction.Add) 
+		{
+			// When a collision has been added to the collision collection, we know an object has 'entered' our trigger
+			if(playerCollider.Entity.Name == "PlayerCharacter")
 			{
 				Entity.DestroyEntity();
 			}
-        }
-    }
+		}
+	}
 }
