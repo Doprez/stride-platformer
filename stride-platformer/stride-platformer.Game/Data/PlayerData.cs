f9abbfe61965b211fd751337ce98fc767ee2d0cd
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-16 19:02:19 -0700

    added new player data that will be used for game state at some point

---
+++ stride-platformer/stride-platformer.Game/Data/PlayerData.cs
@@ -0,0 +1,17 @@
+
+
+using Stride.Engine;
+
+namespace StridePlatformer.Data;
+
+public class PlayerData : StartupScript
+{
+
+	public float PlayerHealth { get; set; } = 100;
+	
+	public override void Start() 
+	{
+		base.Start();
+		
+	}
+}
