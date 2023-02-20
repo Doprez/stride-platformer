b934ebc8e38b95a5914fffe70238106941d2f193
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-19 18:49:40 -0700

    added idlestate WIP

---
+++ stride-platformer/stride-platformer.Game/Core/AI/States/IdleState.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Doprez.Stride.AI.FSMs;
+
+namespace StridePlatformer.States;
+
+public class IdleState : FSMState
+{
+	public override Task EnterState()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public override Task ExitState()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public override Task UpdateState()
+	{
+		throw new System.NotImplementedException();
+	}
+}
