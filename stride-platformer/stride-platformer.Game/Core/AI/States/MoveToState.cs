da48045d242a91402f738a8b048e028cdf303f25
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-21 19:55:39 -0700

    Its finally animated... poorly.. but it is

--- stride-platformer/stride-platformer.Game/Core/AI/States/MoveToState.cs
+++ stride-platformer/stride-platformer.Game/Core/AI/States/MoveToState.cs
@@ -1,21 +1,27 @@
 using System.Threading.Tasks;
 using Doprez.Stride.AI.FSMs;
 using Stride.Core.Mathematics;
+using Stride.Engine;
+
+namespace StridePlatformer.States;
 
 public class MoveToState : FSMState
 {
     public Vector3 Target;
 
-    private Pathfinder _pathfinder;
+    private readonly Pathfinder _pathfinder;
+	private readonly AnimationComponent _animationComponent;
 
-    public MoveToState(Pathfinder pathfinder)
+    public MoveToState(Pathfinder pathfinder, AnimationComponent animationComponent)
     {
         _pathfinder = pathfinder;
+        _animationComponent = animationComponent;
     }
 
     public override async Task EnterState()
     {
         await _pathfinder.SetWaypointAsync(Target);
+		_animationComponent.Play("Walk");
     }
 
     public override Task ExitState()
