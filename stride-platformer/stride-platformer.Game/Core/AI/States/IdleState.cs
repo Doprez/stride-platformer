929f66a2c828062cf57de8b36681705f50419439
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-21 19:55:11 -0700

    Created IdleState added player seen trigger changed state transition to be used in states instead of FSM update

--- stride-platformer/stride-platformer.Game/Core/AI/States/IdleState.cs
+++ stride-platformer/stride-platformer.Game/Core/AI/States/IdleState.cs
@@ -1,22 +1,63 @@
+using System.Collections.Specialized;
 using System.Threading.Tasks;
 using Doprez.Stride.AI.FSMs;
+using Stride.Core.Collections;
+using Stride.Engine;
+using Stride.Physics;
 
 namespace StridePlatformer.States;
 
 public class IdleState : FSMState
 {
+
+	public PhysicsComponent PlayerSeenTrigger;
+
+	private AnimationComponent _animationComponent;
+	private MoveToState _moveTo;
+
+	public IdleState(AnimationComponent animationComponent, MoveToState moveTo)
+	{
+		_animationComponent = animationComponent;
+		_moveTo = moveTo;
+	}
+
 	public override Task EnterState()
 	{
-		throw new System.NotImplementedException();
+		_animationComponent.Play("Idle");
+		PlayerSeenTrigger.Collisions.CollectionChanged += CollisionsChanged;
+		return Task.CompletedTask;
 	}
 
 	public override Task ExitState()
 	{
-		throw new System.NotImplementedException();
+		PlayerSeenTrigger.Collisions.CollectionChanged -= CollisionsChanged;
+		return Task.CompletedTask;
 	}
 
 	public override Task UpdateState()
 	{
-		throw new System.NotImplementedException();
+		return Task.CompletedTask;
 	}
+
+	private void CollisionsChanged(object sender, TrackingCollectionChangedEventArgs args)
+	{
+		// Cast the argument 'Item' to a collision object
+		var collision = (Collision)args.Item;
+
+		// We need to make sure which collision object is not the Trigger collider
+		// We perform a little check to find the ballCollider 
+		var playerCollider = PlayerSeenTrigger == collision.ColliderA ? collision.ColliderB : collision.ColliderA;
+
+		if (args.Action == NotifyCollectionChangedAction.Add)
+		{
+			// When a collision has been added to the collision collection, we know an object has 'entered' our trigger
+			if (playerCollider.Entity.Name == "PlayerCharacter")
+			{
+				var entitycollided = playerCollider.Entity;
+				_moveTo.Target = entitycollided.Transform.WorldMatrix.TranslationVector;
+				FiniteStateMachine.SetCurrentState(_moveTo);
+			}
+		}
+	}
+	
 }
