25f0ddb1b4003a5558d70d605a72bedf2e01f03b
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-25 13:56:33 -0700

    added custom FSM will add to doprez.stride.ai soon

---
+++ stride-platformer/stride-platformer.Game/Core/AI/FSM/FSM.cs
@@ -0,0 +1,58 @@
+﻿using Stride.Core;
+using Stride.Engine;
+using System.Collections.Generic;
+
+namespace Doprez.Stride.AI.FSMs;
+public abstract class FSM : SyncScript
+{
+
+	/// <summary>
+	/// All states that can be accessed by the FSM
+	/// </summary>
+	[DataMemberIgnore]
+	public Dictionary<int, FSMState> States = new();
+
+	protected FSMState? currentState;
+
+	public override void Update()
+	{
+		UpdateFSM();
+		currentState?.UpdateState();
+	}
+
+	public void Add(int id, FSMState state)
+	{
+		States.Add(id, state);
+	}
+
+	public abstract void UpdateFSM();
+
+	public FSMState GetActiveState()
+	{
+		return currentState;
+	}
+
+	public FSMState GetState(int id)
+	{
+		return States[id];
+	}
+
+	public void SetCurrentState(FSMState state)
+	{
+		currentState?.ExitState();
+
+		currentState = state;
+
+		currentState?.EnterState();
+	}
+
+	public void SetCurrentState(int stateIndex)
+	{
+		currentState?.ExitState();
+
+		currentState = GetState(stateIndex);
+
+		currentState?.EnterState();
+	}
+
+}
---
+++ stride-platformer/stride-platformer.Game/Core/AI/FSM/FSMState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Doprez.Stride.AI.FSMs;
+public abstract class FSMState
+{
+	public string Name { get; set; } = Guid.NewGuid().ToString();
+	public FSM? FiniteStateMachine { get; set; }
+	public bool IsDefaultState { get; set; }
+
+	public abstract void EnterState();
+	public abstract void ExitState();
+	public abstract void UpdateState();
+}
