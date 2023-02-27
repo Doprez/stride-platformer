38fe10a30a88d057c7111f719879adf09842cbbc
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-26 18:32:35 -0700

    added myra renderer for my UI

---
+++ stride-platformer/stride-platformer.Game/UI/MyraRenderer.cs
@@ -0,0 +1,40 @@
+﻿using Myra;
+using Myra.Graphics2D.UI;
+using Stride.Engine;
+using Stride.Games;
+using Stride.Graphics;
+using Stride.Rendering;
+using Stride.Rendering.Compositing;
+using UI.Views;
+
+namespace UI;
+public class MyraRenderer : SceneRendererBase
+{
+
+	private MainView _mainView;
+	private Desktop _desktop;
+
+	protected override void InitializeCore()
+	{
+		base.InitializeCore();
+
+		MyraEnvironment.Game = (Game)Services.GetService<IGame>();
+
+		_mainView = new MainView();
+		Services.AddService(_mainView);
+
+		_desktop = new()
+		{
+			Root = _mainView,
+		};
+	}
+
+	protected override void DrawCore(RenderContext context, RenderDrawContext drawContext)
+	{
+		// Clear depth buffer
+		drawContext.CommandList.Clear(GraphicsDevice.Presenter.DepthStencilBuffer, DepthStencilClearOptions.DepthBuffer);
+
+		// Render UI
+		_desktop.Render();
+	}
+}
