932331af641ddd4a89d394852f6eca507c43ebdb
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-26 18:32:51 -0700

    added main view with health bar

---
+++ stride-platformer/stride-platformer.Game/UI/Views/MainView.cs
@@ -0,0 +1,26 @@
+﻿using Myra.Graphics2D.Brushes;
+using Myra.Graphics2D.UI;
+
+namespace UI.Views;
+public class MainView : Panel
+{
+	public HorizontalProgressBar HealthBar;
+
+	public MainView()
+	{
+		HealthBar = new()
+		{
+			HorizontalAlignment = HorizontalAlignment.Left,
+			VerticalAlignment = VerticalAlignment.Bottom,
+			Value = 100,
+			Filler = new SolidBrush("#4BD961FF"),
+			Left = 20,
+			Top = -20,
+			Width = 300,
+			Height = 20,
+			Background = new SolidBrush("#202020FF"),
+		};
+
+		Widgets.Add(HealthBar);
+	}
+}
