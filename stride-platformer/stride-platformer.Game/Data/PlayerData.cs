9a9bef2e337a35a6c8b0a0d7d115b015838b4953
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-26 18:33:08 -0700

    updated player data to update the ui when damage is taken

--- stride-platformer/stride-platformer.Game/Data/PlayerData.cs
+++ stride-platformer/stride-platformer.Game/Data/PlayerData.cs
@@ -1,16 +1,15 @@
-
-
 using Stride.Engine;
 using StridePlatformer.Services;
+using UI.Views;
 
 namespace StridePlatformer.Data;
 
 public class PlayerData : StartupScript
 {
 
-	public float PlayerHealth { get; set; } = 100;
-
+	private float _playerHealth { get; set; } = 100;
 	private GameStateService _gameState;
+	private MainView _mainView;
 	
 	public override void Start() 
 	{
@@ -18,5 +17,28 @@ public class PlayerData : StartupScript
 
 		_gameState = Services.GetService<GameStateService>();
 		_gameState.Player = Entity;
+
+		Services.ServiceAdded += ServiceAdded;
+	}
+
+	private void ServiceAdded(object sender, Stride.Core.ServiceEventArgs e)
+	{
+		if (e.ServiceType == typeof(MainView))
+		{
+			_mainView = Game.Services.GetService<MainView>();
+		}
+	}
+
+	public void RemovePlayerHealth(float damage)
+	{
+		_playerHealth -= damage;
+		//need to change game state when health hits 0
+		_mainView.HealthBar.Value = _playerHealth;
+	}
+
+	public void AddPlayerHealth(float health)
+	{
+		_playerHealth += health;
+		_mainView.HealthBar.Value = _playerHealth;
 	}
 }
